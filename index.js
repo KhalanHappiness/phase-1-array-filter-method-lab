9d9a74cc835e236eaf641cca8611b859efd755a1
happiness <happinesskhalan@gmail.com>
2025-03-11 13:17:11 +0300

    Adds the filter function concept

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+
+function findMatching(drivers, name ){
+
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+
+}
+
+// console.log(findMatching(drivers, 'Sammy'))
+
+function fuzzyMatch(drivers, letter ){
+
+    return drivers.filter(driverName => driverName.slice(0,2) === letter)
+
+}
+
+console.log(fuzzyMatch(drivers, 'Sa'))
+
+const driver = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+function matchName(driver, Dname){
+
+    return driver.filter(driverDetails => driverDetails.name === Dname)
+
+}
+console.log(driver, "Annette")
